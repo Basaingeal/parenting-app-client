dfc3b7935413ec3aa286bff4c72ce1b68ace2f34
Basaingeal <michaeljcurrie136@gmail.com>
2019-01-04 00:02:59 -0600

    Immport bootstrap-vue and auth0 libraries

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import { describe, it } from '@vue/cli-plugin-unit-mocha'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
