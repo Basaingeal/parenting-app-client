65ea77fe91f74d7f1b8d69b641478b3561fc6e73
Michael j. Currie <mcurrie@agwsinc.com>
2019-02-07 11:02:37 -0600

    service worker improvements

--- vue.config.js
+++ vue.config.js
@@ -15,59 +15,27 @@ module.exports = {
     themeColor: '#FFFFFF',
     msTileColor: '#ea80fc',
     workboxOptions: {
-      runtimeCaching: [{
-        urlPattern: /\.(?:js|css)$/,
-        handler: 'cacheFirst',
-        options: {
-          cacheName: 'js-and-css',
-          expiration: {
-            maxEntries: 25
+      runtimeCaching: [
+        {
+          urlPattern: /graphql/,
+          handler: 'networkFirst',
+          options: {
+            cacheName: 'graphql',
+            backgroundSync: {
+              name: 'graphql-queue'
+            }
           }
-        }
-      },
-      {
-        urlPattern: /\.(?:html|.app)$/,
-        handler: 'networkFirst',
-        options: {
-          networkTimeoutSeconds: 10,
-          cacheName: 'html',
-          expiration: {
-            maxEntries: 2,
-            purgeOnQuotaError: true
+        },
+        {
+          urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+          handler: 'staleWhileRevalidate',
+          options: {
+            cacheName: 'images',
+            expiration: {
+              purgeOnQuotaError: true
+            }
           }
-        }
-      },
-      {
-        urlPattern: /graphql/,
-        handler: 'networkFirst',
-        options: {
-          cacheName: 'graphql',
-          backgroundSync: {
-            name: 'graphql-queue'
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
-        handler: 'staleWhileRevalidate',
-        options: {
-          cacheName: 'images',
-          expiration: {
-            maxEntries: 10,
-            purgeOnQuotaError: true
-          }
-        }
-      },
-      {
-        urlPattern: /\.(?:woff|woff2|ttf|otf)$/,
-        handler: 'cacheFirst',
-        options: {
-          cacheName: 'fonts',
-          expiration: {
-            maxEntries: 50
-          }
-        }
-      }]
+        }]
     }
   }
 }
